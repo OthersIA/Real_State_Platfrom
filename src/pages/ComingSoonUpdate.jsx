53890350c8a1363521665253b6e2f282fc4522cf
yeasin-islam <info.yeasinislam@gmail.com>
2025-08-17 12:12:20 +0600

    feat: Add Helmet for SEO optimization in ComingSoonUpdate component

--- src/pages/ComingSoonUpdate.jsx
+++ src/pages/ComingSoonUpdate.jsx
@@ -1,6 +1,7 @@
 // src/components/ComingSoon.jsx
-import React from 'react'; 
-import  { useNavigate } from "react-router";
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useNavigate } from "react-router";
 
 const ComingSoonUpdate = ({
   title = "Coming Soon",
@@ -11,6 +12,10 @@ const ComingSoonUpdate = ({
 
   return (
     <div className="flex items-center justify-center  min-h-[calc(100vh-64px)] px-4 bg-gradient-to-r from-base-100 to-base-500 dark:from-gray-800 dark:to-gray-900">
+      <Helmet>
+        <title>All Properties | ComingSoon</title>
+      </Helmet>
+
       <div className="text-center bg-base-300 dark:bg-gray-800/80 p-10 rounded-2xl shadow-xl backdrop-blur-md">
         <h1 className="text-4xl md:text-5xl font-bold text-[#00BBA7] mb-4">
           🚧 {title}
